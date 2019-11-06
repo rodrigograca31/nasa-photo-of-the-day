7163e2c48b2e62bd1bddfad69cc6c5f06a1ab09d
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-06 11:53:55 +0000

    Formatting :card_file_box:

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>
+				Read through the instructions in the README.md file to build your NASA
+				app! Have fun 🚀!
+			</p>
+		</div>
+	);
 }
 
 export default App;
