02583fed2818fa303faf60894931e0130ae7f608
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-07 11:30:30 +0000

    Stretch :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,36 +1,94 @@
 import React from "react";
-import axios from "axios";
-import "./App.css";
+import "./App.scss";
 import APODComp from "./components/APOD";
+import axios from "axios";
+
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+// import { } from "@material-ui/core/";
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers/';
+import DateFnsUtils from '@date-io/date-fns';
+import { setDay } from "date-fns";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: purple
+	}
+});
 
 function App() {
 
 	const [APOD, setAPOD] = React.useState({})
+	const [date, setDate] = React.useState(new Date())
+
+	const [days, setDays] = React.useState(0)
 
 	React.useEffect(() => {
 		// https://api.nasa.gov/planetary/apod?api_key=tsGCE22GDvhxJbssPpCtznaQVPqBI3QdAD71Zufp
-		console.log(axios);
+		console.log(date.toISOString().slice(0, 10));
 
-		axios.get("https://api.nasa.gov/planetary/apod?api_key=tsGCE22GDvhxJbssPpCtznaQVPqBI3QdAD71Zufp")
-		.then((response)=>{
+		// const newDate = (new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * (days))).toISOString().slice(0, 10);
+		// setDate(newDate)
+
+		axios.get("https://api.nasa.gov/planetary/apod?api_key=tsGCE22GDvhxJbssPpCtznaQVPqBI3QdAD71Zufp&date=" + date.toISOString().slice(0, 10))
+		.then((response) => {
 			console.log(response);
 			return response.data;
-		}).then((data)=> {
+		}).then((data) => {
 			setAPOD(data)
 		})
 
-		
 		console.log("once");
-		
-		
-	}, []);
+
+	}, [date]);
+
+	const newDate = (dir) => {
+		console.log("new date");
+		setAPOD({});
+		if (dir === "prev") {
+			setDate(new Date(date.getTime() - 1000 * 60 * 60 * 24));
+		} else {
+			setDate(new Date(date.getTime() + 1000 * 60 * 60 * 24));
+		}
+	}
+
+	const dateChanged = (date) => {
+		console.log(date);
+		setAPOD({});
+		setDate(date)
+	}
 
 	return (
-		<div className="App">
-			<p>🚀 🚀 🚀</p>
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<p>🚀 🚀 🚀</p>
+				<div className="buttons">
+					<Button variant="contained" color="primary" onClick={() => newDate("prev")}>⬅️ Prev Dat</Button>
+					
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<KeyboardDatePicker
+							disableToolbar
+							variant="inline"
+							format="MM/dd/yyyy"
+							margin="normal"
+							id="date-picker-inline"
+							label="Date picker inline"
+							value={date}
+							KeyboardButtonProps={{
+								'aria-label': 'change date',
+							}}
+							onChange={dateChanged}
+						/>
+					</MuiPickersUtilsProvider>
 
-			<APODComp data={APOD} />
-		</div>
+					<Button variant="contained" color="primary" onClick={() => newDate("next")}>Next Date ➡️</Button>
+				</div>
+				{
+					JSON.stringify(APOD) !== "{}" && <APODComp setAPOD={setAPOD} data={APOD} />
+				}
+			</div>
+		</ThemeProvider>
 	);
 }
 
--- src/components/APOD.js
+++ src/components/APOD.js
@@ -3,13 +3,36 @@ import "./APOD.scss";
 
 const APODComp = (props) => {
 
+	React.useEffect(() => {
+		return (() => {
+			setLoading(true);
+			console.log("unmount");
+		})
+	}, []);
+
+	const [loading, setLoading] = React.useState(true);
+
+	React.useEffect(()=> {
+		setLoading(true);
+	}, [props.data.hdurl]);
+
+	const finishedLoading = () => {
+		console.log("f l");
+		setLoading(false);
+	}
+
 	return (
 		<div id="data">
 			{/* {JSON.stringify(props.data)} */}
 			<h1>{props.data.title}</h1>
 			{/* I should deal with 'media_types' but Im lazy */}
-			<img src={props.data.url} alt={props.data.title}/>
-			<p>{props.data.explanation}</p>
+			<div>
+				<div className="spinner">
+					{loading && <img src="./imgs/spinner.png" alt="" />}
+				</div>
+				<img src={props.data.hdurl} alt={props.data.title} onLoad={() => { finishedLoading() }} />
+			</div>
+			<p>{props.data.explanation.slice(0, 200)}......</p>
 		</div>
 	);
 }
